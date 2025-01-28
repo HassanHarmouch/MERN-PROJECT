5c1f93b723935d0f803148d91c430adea3ca11ae
DianaFarhat <dsf04@mail.aub.edu>
2025-01-28 23:42:28 +0200

    Establish DB_Connection

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+const express= require('express');
+const { connectToDatabase } = require('./database');
+const app= express();
+const DB= require('./database').connectToDatabase;
+
+// Run the database connection
+connectToDatabase();
+
+app.use(express.json());
+
+//Set up port
+app.listen(3000, ()=>{
+    console.log('Listening on port 3000')
+})
