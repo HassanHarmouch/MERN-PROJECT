6bb0a16bf0824e93ff04c48c00b87a322280b01a
DianaFarhat <dsf04@mail.aub.edu>
2025-01-28 23:53:03 +0200

    Add User Model

---
+++ backend/models/userModel.js
@@ -0,0 +1,63 @@
+const mongoose= require("mongoose");
+const { trim } = require("validator");
+const Schema= mongoose.Schema;
+const bcrypt= require('bcrypt');
+
+const userSchema= new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: [true, "Please enter your first name"],
+        minLength: 3,
+        trim: 2,
+    },
+    lastName:{
+        type: String,
+        required: [true, "Please enter your last name"],
+        minLength: 3,
+        trim: 2,
+    }, 
+    email:{
+        type: String,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        required: [true, "Please enter your email"],
+    },
+    username:{
+        type: String,
+        unique: true,
+        trim: true,
+        required: [true, "Please enter your username"],
+    },
+    password: {
+        type: String,
+        minLength: 8,
+        trim: true,
+        required: [true, "Please enter your password"],
+    },
+    passwordConfirm: {
+        type: String,
+        minLength: 8,
+        trim: true,
+        required: [true, "Please confirm your password"],
+    },
+    passwordChangedAt: Date,
+
+    role: {
+        type: String,
+        default: "user",
+        enum: ["admin", "user"],
+    },
+    
+    orders: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Order",
+        }
+    ],
+},
+{timestamps: true}
+)
+
+
+module.exports= mongoose.model("User", userSchema);
