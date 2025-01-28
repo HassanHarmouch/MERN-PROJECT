c56bca2b5d1e8564a2ad5dded3bb74b29bc9810c
DianaFarhat <dsf04@mail.aub.edu>
2025-01-29 00:02:32 +0200

    Fix User Model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,6 @@
 const mongoose= require("mongoose");
-const { trim } = require("validator");
 const Schema= mongoose.Schema;
-const bcrypt= require('bcrypt');
+
 
 const userSchema= new mongoose.Schema({
     firstName:{
